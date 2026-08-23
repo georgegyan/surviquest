b67dfce09f966c595c804b63759c8fb9d81e5981
georgegyan <georgegyankwame72@gmail.com>
2026-08-23 22:45:13 +0000

    feat: add question serializers

---
+++ backend/apps/questions/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import ( Question, QuestionOption )
+
+class QuestionOptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuestionOption
+        fields = [
+            "id",
+            "option_text",
+            "order",
+        ]
+
+class QuestionSerializer(serializers.ModelSerializer):
+    options = QuestionOptionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Question
+        fields = [
+            "id",
+            "survey",
+            "question_text",
+            "question_type",
+            "is_required",
+            "order",
+            "settings",
+            "options",
+            "created_at",
+        ]
