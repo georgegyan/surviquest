a8172ecd0a398ba5ef9771176df49777ecf9ceb4
georgegyan <georgegyankwame72@gmail.com>
2026-08-24 16:16:29 +0000

    feat: add question option endpoints

--- backend/apps/questions/urls.py
+++ backend/apps/questions/urls.py
@@ -4,6 +4,10 @@ from .views import (
     QuestionListView,
     QuestionUpdateView,
     QuestionDeleteView,
+    OptionListView,
+    OptionCreateView,
+    OptionUpdateView,
+    OptionDeleteView
 )
 
 urlpatterns = [
@@ -11,4 +15,8 @@ urlpatterns = [
     path("survey/<int:survey_id>/", QuestionListView.as_view(), name="question-list"),
     path("<int:pk>/update/", QuestionUpdateView.as_view(), name="update-question"),
     path("<int:pk>/delete/", QuestionDeleteView.as_view(), name="delete-question"),
+    path("<int:question_id>/options/", OptionListView.as_view(), name="option-list"),
+    path("options/create/", OptionCreateView.as_view(), name="create-option"),
+    path("options/<int:pk>/update/", OptionUpdateView.as_view(), name="update-option"),
+    path("options/<int:pk>/delete/", OptionDeleteView.as_view(), name="delete-option"),
 ]
--- backend/apps/questions/views.py
+++ backend/apps/questions/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import (IsAuthenticated)
-from .models import Question
-from .serializers import (QuestionSerializer)
+from .models import Question, QuestionOption
+from .serializers import (QuestionSerializer, QuestionOptionSerializer)
 
 class QuestionCreateView(generics.CreateAPIView):
     serializer_class = QuestionSerializer
@@ -24,3 +24,24 @@ class QuestionUpdateView(generics.UpdateAPIView):
 class QuestionDeleteView(generics.DestroyAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Question.objects.all()
+
+class OptionCreateView(generics.CreateAPIView):
+    serializer_class = (QuestionOptionSerializer)
+    permission_classes = [IsAuthenticated]
+
+class OptionListView(generics.ListAPIView):
+    serializer_class = (QuestionOptionSerializer)
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        question_id = self.kwargs["question_id"]
+        return QuestionOption.objects.filter(question_id=question_id).order_by("order")
+
+class OptionUpdateView(generics.UpdateAPIView):
+    serializer_class = (QuestionOptionSerializer)
+    permission_classes = [IsAuthenticated]
+    queryset = QuestionOption.objects.all()
+
+class OptionDeleteView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = QuestionOption.objects.all()
